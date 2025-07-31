6f5c13e1a9ed4805a3243e6b1a4fdd187c12a03f
Punam <punamkumari399@gmail.com>
2025-07-31 18:59:34 +0530

    meta data

--- src/components/Trust.jsx
+++ src/components/Trust.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react";
 const Trust = () => {
   return (
     <div className="mt-[10%] flex flex-col gap-4 items-center w-full h-full mb-[10%]">
@@ -11,25 +12,49 @@ const Trust = () => {
         </h3>
       </div>
       <div className="mt-20 flex flex-wrap justify-between gap-4 w-full">
-        <img
+        <motion.img
           src="/images/Netflix-logo.jpg"
           alt="netflix"
-          className=" w-15 h-10 md:w-40 animate-float "
+          className="w-15 h-10 md:w-40"
+          animate={{ y: [0, -15, 0] }}
+          transition={{
+            duration: 9000,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
         />
-        <img
+        <motion.img
           src="/images/googlelogo2.png"
           alt="google"
           className="mix-blend-multiply  w-15 h-10 hover:scale-110 md:w-30"
+          animate={{ y: [0, -15, 0] }}
+          transition={{
+            duration: 9000,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
         />
-        <img
+        <motion.img
           src="/images/Meta-Logo.png"
           alt="meta"
           className=" w-15 h-10 md:w-40"
+          animate={{ y: [0, -15, 0] }}
+          transition={{
+            duration: 9000,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
         />
-        <img
+        <motion.img
           src="/images/onlyfans2.png"
           alt="onlyfans"
           className=" w-15 h-10 md:w-30"
+          animate={{ y: [0, -15, 0] }}
+          transition={{
+            duration: 9000,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
         />
       </div>
     </div>
